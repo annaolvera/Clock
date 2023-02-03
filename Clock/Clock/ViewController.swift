0d58a0831e8cbd24da761a543995696182aaf35b
Anna Olvera <97631205+annaolvera@users.noreply.github.com>
2023-02-02 21:04:41 -0600

    Version 2
    Live Clock, Timer Picker, and Remaining Time is displaying when selected. Start/stop button is still not implemented because I am unable to connect the button to the UIDatePicker yet. Still working on errors.

--- Clock/Clock/ViewController.swift
+++ Clock/Clock/ViewController.swift
@@ -9,28 +9,64 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //current time
+    //current live time label
     @IBOutlet weak var currentTimeLabel: UILabel!
     
+    // remaining time label
+    @IBOutlet weak var timeRemaining: UILabel!
+    
+ 
+    
+    
+    //variables
     var timer = Timer()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //Do any additional setuo after loading the view.
+    
         
         getCurrentTime()
+        
     }
     
-    //Format Time
+    //Get live Time
     private func getCurrentTime() {
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:#selector(self.currentTime) , userInfo: nil, repeats: true)
     }
     
+    //Format Live Time
     @objc func currentTime() {
         let formatter = DateFormatter();
         formatter.dateFormat = "E, d MMM yyyy HH:mm:ss"
         currentTimeLabel.text = formatter.string(from: Date())
         
     }
-}
+    
+    //UIDatePicker
+    @IBAction func timerPicker(_ sender: UIDatePicker) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm:ss"
+        timeRemaining.text = formatter.string(from: sender.date)
+            print("Timer fired!")
+    
+        }
+    
+    //@IBAction func startStopButton(_ sender: UIButton) {
+    //    for timeRemaining in timerPicker-> () {
+    //        timeRemaining.stop()
+    //        timeRemaining.currentTime = 0
+    //    }
+        
+    //}
+    
+        
+    }
+    
+
+
+    
+    
+    
+
 
